5e0faf25f5d421ee3b3bbee07ccf277406efac26
Amit Kotlovski <amitbk@gmail.com>
2018-01-08 17:12:16 +0200

    Arranging code + adding README file

---
+++ README.md
@@ -0,0 +1,15 @@
+# Sanity Price Monitor
+Determines 
+
+# Installing
+## Clone the repo
+This repo makes use of the [smart-contracts](https://github.com/KyberNetwork/smart-contracts) as a submodule for it's 
+configuration, so to clone this repo run:
+    
+    $ git clone https://github.com/KyberNetwork/sanity-price-monitor.git --recursive
+    
+## Installing library dependencies
+Enter the folder and use pipenv to install library dependencies:
+
+    $ cd sanity-price-monitor
+    $ pipenv install
--- pricemonitor/monitor.py
+++ pricemonitor/monitor.py
@@ -3,47 +3,7 @@ import logging
 
 import yaml
 
-from pricemonitor.datasource.exchanges import Exchange, ExchangeName
-
-
-class ExchangePriceMonitor:
-    def __init__(self, coins, market):
-        self._coins = coins
-        self._market = market
-        self._exchanges = [
-            Exchange.get_exchange(name)
-            for name in [ExchangeName.BINANCE, ExchangeName.BITTREX]
-        ]
-
-    async def monitor(self, loop):
-        coin_prices = await self._get_coin_prices(self._coins, self._market, loop)
-
-        from pprint import pprint
-        pprint(coin_prices)
-
-    async def _get_coin_prices(self, coins, market, loop):
-        coin_prices_calculations = [
-            self._get_average_price(coin, market, loop)
-            for coin in coins
-        ]
-        coin_prices = await asyncio.gather(
-            *(price_calculation for price_calculation in coin_prices_calculations), loop=loop)
-        return coin_prices
-
-    # TODO: filter pairs that are not traded on the exchange
-    async def _get_average_price(self, coin, market, loop):
-        price_calls = (
-            exchange.get_last_trades_average_or_last_trade(coin=coin, market=market)
-            for exchange in self._exchanges
-        )
-        prices_values = [
-            price
-            for price in await asyncio.gather(*price_calls, loop=loop)
-            if price is not None
-        ]
-        average_price = sum(prices_values) / len(prices_values)
-
-        return (coin, market), average_price
+from pricemonitor.monitoring.exchangeprices import ExchangePriceMonitor
 
 
 async def main(loop):
